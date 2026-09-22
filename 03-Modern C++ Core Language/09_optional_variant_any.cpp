4c6e1d9c54c319051d377eee94b00e00309bf172
Pratyush Kumar <kumarpratyush574@gmail.com>
2026-09-23 01:18:40 +0530

    variant in cpp

--- 03-Modern C++ Core Language/09_optional_variant_any.cpp
+++ 03-Modern C++ Core Language/09_optional_variant_any.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<optional>
+#include<variant>
+#include<any>
+
 using namespace std;
 
 optional<int> getNumber(bool found){
@@ -10,12 +13,23 @@ optional<int> getNumber(bool found){
 }
 
 int main(){
-
+	// optional means "a value may or may not exist."
 	optional<int> result = getNumber(true);
 	if(result.has_value()){
 		cout<<result.value();
 	}else{
 		cout<<"not found";
 	}
+	cout<<endl;
+	//Varient means:Holds one of the predefined types (int or string).
+	variant<int,string> v=10;
+	cout<<get<int>(v)<<endl;
+	v="hello";
+	cout<<get<string>(v)<<endl;
+    
+  
+
+
+
 	return 0;
 }
